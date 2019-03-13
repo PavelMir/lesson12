fdbd89e2261c55dcce4686d9f3bef797cd9e6f74
PavelMir <45740857+PavelMir@users.noreply.github.com>
2019-03-13 16:26:40 +0200

    Update related.sql

--- related.sql
+++ related.sql
@@ -27,8 +27,7 @@ UPDATE products SET related_id = ABS(id - 2);
 SELECT p.id, p.name, p.price, p.description,
        r.name related_name, r.price related_price
   FROM products p
-  JOIN products r on r.id = p.related_id
-WHERE p.id > 3 AND p.id < 8; 
+  JOIN products r on r.id = p.related_id; 
 
 --Get product details with related product name and price. Columns in result:
 --id, name, price, description, related_name, related_price
