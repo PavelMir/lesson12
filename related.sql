156c3c16a6c6bccd90ad4ac7267bd7bb14156820
PavelMir <45740857+PavelMir@users.noreply.github.com>
2019-03-13 07:42:01 +0200

    Update related.sql

--- related.sql
+++ related.sql
@@ -1,9 +1,8 @@
 SELECT p.id, p.name, p.price, p.description,
-       r.name as related_name, r.price as related_price
-  FROM products as p
-  JOIN products as r on r.id = p.related_id
+       r.name related_name, r.price related_price
+  FROM products p
+  JOIN products r on r.id = p.related_id
 WHERE p.id > 3 AND p.id < 8; 
- 
 
 --Get product details with related product name and price. Columns in result:
 --id, name, price, description, related_name, related_price
